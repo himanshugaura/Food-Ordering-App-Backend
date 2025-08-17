c18b25a3963b919c45f04c1b00694b1625daa372
himanshu <himanshu32505@gmail.com>
2025-08-17 13:47:09 +0530

    definig constants

---
+++ src/constants.ts
@@ -0,0 +1,17 @@
+export enum OrderStatus {
+  Pending = "PENDING",
+  Processing = "PROCESSING",
+  Shipped = "SHIPPED",
+  Delivered = "DELIVERED",
+  Cancelled = "CANCELLED"
+}
+
+export enum FoodType {
+  Veg = "VEG",
+  NonVeg = "NON VEG"
+}
+
+export enum PaymentMethod {
+  Cash = "CASH",
+  Online = "ONLINE"
+}
