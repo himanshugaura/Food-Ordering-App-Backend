53705642d0b4629eab998543bd6fc078eb76e29e
himanshu <himanshu32505@gmail.com>
2025-10-02 22:46:22 +0530

    update in order status

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
 export enum OrderStatus {
-  Pending = "PENDING",
-  Processing = "PROCESSING",
-  Shipped = "SHIPPED",
-  Delivered = "DELIVERED",
-  Cancelled = "CANCELLED"
+  PENDING = "PENDING",       // Order placed, awaiting store confirmation
+  COOKING = "COOKING",       // Order accepted, being prepared
+  DELIVERED = "DELIVERED",   // Order completed and given to customer
+  CANCELLED = "CANCELLED"    // Order cancelled
 }
+
 
 export enum FoodType {
   Veg = "VEG",
--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -1,69 +1,69 @@
-import mongoose, { model, Schema, Types } from "mongoose";
-import type { IOrders } from "../types/type.js";
-import { OrderStatus, PaymentMethod } from "../constants.js";
+  import mongoose, { model, Schema, Types } from "mongoose";
+  import type { IOrders } from "../types/type.js";
+  import { OrderStatus, PaymentMethod } from "../constants.js";
 
-const OrdersSchema = new Schema<IOrders>(
-  {
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-
-    orderItems: [
-      {
-        product: {
-          type: Schema.Types.ObjectId,
-          ref: "Product",
-          required: true,
-        },
-
-        quantity: {
-          type: Number,
-          required: true,
-          trim: true,
-        },
-
-        price: {
-          type: Number,
-          required: true,
-          trim: true,
-        },
+  const OrdersSchema = new Schema<IOrders>(
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
       },
-    ],
 
-    orderNo: {
-      type: Number,
-      required: true,
+      orderItems: [
+        {
+          product: {
+            type: Schema.Types.ObjectId,
+            ref: "Product",
+            required: true,
+          },
+
+          quantity: {
+            type: Number,
+            required: true,
+            trim: true,
+          },
+
+          price: {
+            type: Number,
+            required: true,
+            trim: true,
+          },
+        },
+      ],
+
+      orderNo: {
+        type: Number,
+        required: true,
+      },
+
+      totalAmount: {
+        type: Number,
+        required: true,
+      },
+
+      status: {
+        type: String,
+        enum: Object.values(OrderStatus),
+        required: true,
+        default: OrderStatus.PENDING,
+      },
+
+      paymentMethod: {
+        type: String,
+        enum: Object.values(PaymentMethod),
+        default: PaymentMethod.Cash,
+      },
+
+      isPaid: { 
+          type: Boolean, 
+          required : true,
+          default: false 
+      },
     },
+    { timestamps: true }
+  );
 
-    totalAmount: {
-      type: Number,
-      required: true,
-    },
-
-    status: {
-      type: String,
-      enum: Object.values(OrderStatus),
-      required: true,
-      default: OrderStatus.Pending,
-    },
-
-    paymentMethod: {
-      type: String,
-      enum: Object.values(PaymentMethod),
-      default: PaymentMethod.Cash,
-    },
-
-    isPaid: { 
-        type: Boolean, 
-        required : true,
-        default: false 
-    },
-  },
-  { timestamps: true }
-);
-
-const OrderModel =
-  mongoose.models.Order || model("Order", OrdersSchema);
-export default OrderModel;
+  const OrderModel =
+    mongoose.models.Order || model("Order", OrdersSchema);
+  export default OrderModel;
