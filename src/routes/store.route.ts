13d1541afe2a1da6a68a69499458e7a7e9fd54f4
himanshu <himanshu32505@gmail.com>
2025-10-15 22:33:26 +0530

    bug fix

--- src/routes/store.route.ts
+++ src/routes/store.route.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { createStore, getOrderCounter, getStoreDetails, getStoreStatus, resetOrderCounter, toggleStoreStatus, updateStoreDetails } from "../controllers/store.controller.js";
+import { upload } from "../middlewares/upload.js";
 
 const storeRouter = express.Router();
 
-storeRouter.post("/create" , authMiddleware , createStore);
+storeRouter.post("/create" , authMiddleware , upload.single("image") , createStore);
 storeRouter.get("/details" , authMiddleware , getStoreDetails);
-storeRouter.patch("/update" , authMiddleware , updateStoreDetails);
-storeRouter.patch("toggle/status" , authMiddleware , toggleStoreStatus);
-storeRouter.get("order-counter" , authMiddleware , getOrderCounter);
-storeRouter.patch("reset/order-counter" , authMiddleware , resetOrderCounter);
+storeRouter.patch("/update" , authMiddleware , upload.single("image") , updateStoreDetails);
+storeRouter.patch("/toggle/status" , authMiddleware , toggleStoreStatus);
+storeRouter.get("/order-counter" , authMiddleware , getOrderCounter);
+storeRouter.patch("/reset/order-counter" , authMiddleware , resetOrderCounter);
 storeRouter.get("/status" , authMiddleware , getStoreStatus);
 
 export default storeRouter;
